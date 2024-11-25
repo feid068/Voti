18358b6eb7bc2772f80ebe36d254a8d21f80883b
feid068 <davide01031972@gmail.com>
2024-11-25 19:24:53 +0100

    creazione menu

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "librerie/funzioni_della_media.h"
 #include "librerie/leggo_e_scrivo_file.h"
 
-# define MAX_SIZE 25
+# define MAX_SIZE 15
 
 float voti[MAX_SIZE];
 float pesi[MAX_SIZE];
@@ -10,21 +10,57 @@ float media[MAX_SIZE];
 char nome[] = "materie.txt";
 char path[] = "materie/";
 char extention[] = ".txt";
+char arrey_mat[MAX_SIZE][1024];
+
+int menu();
 
 int main(){
     FILE *file;
-
-    //printf("Inserisci il nome della materia che vuoi aggiungere\n");
-    //char nomeMat[50];
-    //input(nomeMat, 50);
-    //ins(&file, nome, nomeMat);
-    char a[15][1024];
-    crea_file_materie(&file, nome, path, extention, a);
     
-    prende_i_voti(&file, a[1], voti, pesi);
+    int a = menu();
+
+    while (a != 5){
+        switch (a){
+        case 1:
+            printf("Inserisci il nome della materia\n");
+            char mat[50];
+            fgets(mat, 50, stdin);
+            ins_materie(&file, nome, mat);
+            crea_file_materie(&file, nome, path, extention, arrey_mat);
+            a = menu();
+            break;
+        case 2:
+            a = menu();
+            break;
+        case 3:
+            a = menu();
+            break;
+        case 4:
+            a = menu();
+            break;
+        case 5:
+            return 1;
+            break;
+        default:
+            printf("Valore non validi\n");
+            a = 5;
+            break;
+        } 
+    }
 
     if(file != NULL){
         fclose(file);
     }
     return 0;
+}
+
+int menu(){
+    int input;
+    printf("Benvenuto, cosa vuoi fare?\n");
+    printf("Per aggiungere una materia: 1\n");
+    printf("Per ossevare le materie: 2\n");
+    printf("Esci: 5\n");
+    scanf("%d", &input);
+    fgetc(stdin);
+    return input;
 }
